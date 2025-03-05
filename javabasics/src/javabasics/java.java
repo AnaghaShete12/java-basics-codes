0ebcc04e1d4b047943475e847eb943e73859a8b8
SWIFTPLM-PC-02 <SWIFTPLM-PC-02@SWIFTPLM-PC-02>
2025-03-05 10:42:33 +0530

    first commit

---
+++ javabasics/src/javabasics/java.java
@@ -0,0 +1,209 @@
+package javabasics;
+
+public class java
+{
+public static void main(String[] args)
+{
+	System.out.println("Hi");
+	System.out.println('Y');
+	System.out.println(1);
+	System.out.println(22);
+	System.out.println(333);
+	System.out.println(4444);
+	String name="Ravi";
+	System.out.println(name);
+	char myFirstName='S';
+	System.out.println(myFirstName);
+	boolean iamgood=true;
+	System.out.println(iamgood);
+	boolean iambad=false;
+	System.out.println(iambad);
+	int x=100;
+	int y=200;
+	int z=300;
+	System.out.println(x+y+z);
+	float myfloatingNumber=6.99f;
+	System.out.println(myfloatingNumber);
+	String Name="Teju";
+	System.out.println(name + Name);
+	int a,b,c,d,e,f,g,h,i,j;
+	a=b=c=d=e=f=g=h=i=j=100;
+	System.out.println(a*b+c+d+e+f+g+h*i+j);
+	byte mynumber=111;
+	System.out.println(mynumber);
+	short mynummber=5200;
+	System.out.println(mynummber);
+	long myyNumber=110010191910119L;
+	System.out.println(myyNumber);
+	double mmynumber=12e2d;
+	System.out.println(mmynumber);
+	int myNum=15;
+	myNum=25;
+	System.out.println(myNum);
+	String namee="Tejuuuuu";
+	String fullName="raviiiiii";
+	String goodName=namee + fullName;
+	System.out.println(goodName);
+	int myRank=10;
+	int Myrank=50;
+	int MYRANK=35;
+	System.out.println(myRank+Myrank-MYRANK);
+	int u=90;
+	int v=900;
+	int w=120;
+	System.out.println(v+u);
+	System.out.println(v-u);
+	System.out.println(v*u);
+	System.out.println(v/u);
+	System.out.println(v%u);
+	++u;
+	System.out.println(u);
+	++v;
+	System.out.println(v);
+	++w;
+	System.out.println(w);
+int r=89;
+
+--r;
+System.out.println(r);
+int p=67;
+--p;
+System.out.println(p);
+int l=88;
+--l;
+System.out.println(l);
+int X=90;
+X +=10;
+System.out.println(X);
+int Y=80;
+Y-=10;
+System.out.println(Y);
+int Z=60;
+Z*=10;
+System.out.println(Z);
+int A=20;
+A/=5;
+System.out.println(A);
+int B=30;
+B%=100;
+System.out.println(B);
+int C=10;
+C&=1;
+System.out.println(C);
+int D=60;
+D<<=9;
+System.out.println(D);
+int E=65;
+E>>=100000;
+System.out.println(E);
+int q=32;
+int W=12;
+System.out.println(q>=W);
+String txt="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+System.out.println("The Length of the String Is: " + txt.length());
+System.out.println("Written in lowercase: " + txt.toLowerCase());
+System.out.println("Written in uppercase: " + txt.toUpperCase());
+String text="Hai My Name is tejaaa";
+System.out.println(text.indexOf("is"));
+String FirstTxt="Hi Teja";
+String LastTxt="Hi Ravi";
+System.out.println(FirstTxt+ " " +LastTxt);
+System.out.println(FirstTxt.concat(LastTxt));
+System.out.println(Math.max(10,80));
+System.out.println(Math.min(80,50));
+System.out.println(Math.sqrt(80));
+System.out.println(Math.abs(-9005255));
+System.out.println(Math.random());
+int H=100;
+int J=200;
+if(H>J)
+{
+	System.out.println("IT PRINTS TRUE");
+}
+else
+{
+	System.out.println("IT PRINTS FALSE");
+}
+int R=900;
+int F=800;
+if(R>F)
+{
+	System.out.println("IT prints true");
+}
+else if(R<F)
+{
+	System.out.println("IT prints false");
+}
+else
+{
+	System.out.println("IT prints both false");
+}
+int month=4;
+switch(month)
+{
+case 1:
+	System.out.println("Jan");
+	break;
+case 2:
+	System.out.println("feb");
+	break;
+case 3:
+	System.out.println("mar");
+	break;
+case 4:
+	System.out.println("apr");
+	break;
+	
+}
+int T=0;
+while(T<25)
+{
+	System.out.println(T);
+	T++;
+}
+int t=3;
+while(t<10) {
+	System.out.println(t);
+	t++;
+	
+}
+int U=8;
+while(U<22)
+{
+	System.out.println(U);
+	U++;
+}
+int M=0;
+do
+{
+	System.out.println(M);
+	M++;
+}
+	while(M<50);
+int Q=0;
+do
+{
+	System.out.println(Q);
+	Q++;
+}
+while(Q<10);
+
+
+
+
+
+
+
+}}
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+
